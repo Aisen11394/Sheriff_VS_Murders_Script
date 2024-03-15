4ce098575a72ce2aa6784279bdffcc57a797070d
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-03-15 20:41:31 +0900

    Script.lua

---
+++ .lua
@@ -0,0 +1,31 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("DrRay", "Default")
+
+local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+
+tab.newButton("Button", "Prints Hello!", function()
+    print('Hello!')
+end)
+
+tab.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+
+tab.newInput("Input", "Prints your input.", function(text)
+    print("Entered text: " .. text)
+end)
+
+tab.newKeybind("Input Key", "Press the key to start; it will be printed out.", function(key)
+    print(key)
+end)
+
+tab.newSlider("Slider", "Epic slider", 100, false, function(num)
+    game:GetService("RunService").RenderStepped:wait()
+while task.wait() do
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = num
+    end
+end)
